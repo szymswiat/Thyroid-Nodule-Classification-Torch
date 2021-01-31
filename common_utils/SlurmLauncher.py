4a85236751ff8d13981834064d72a44a318d1f8f
Szymon Światczyński <szymswiat5@gmailc.om>
2021-02-01 00:16:25 +0100

    Encapsulate slurm launcher into class

---
+++ common_utils/SlurmLauncher.py
@@ -0,0 +1,110 @@
+from abc import abstractmethod
+from argparse import ArgumentParser
+
+from test_tube import SlurmCluster, HyperOptArgumentParser
+
+
+class SlurmLauncher:
+    AVAILABLE_PARTITIONS = ['plgrid-gpu', 'plgrid-gpu-v100']
+
+    def __init__(self, args):
+        self._args = args
+        self._parser = HyperOptArgumentParser(strategy='grid_search')
+
+    def setup_cluster_params(self, parser: ArgumentParser) -> None:
+        """
+        Setups basic options for slurm task launcher.
+        :param parser: Argument parser.
+        :return: None
+        """
+        # cluster resources
+        # assuming that single experiment is executed on single node
+        parser.add_argument('-n', '--nodes_per_exp', type=int, default=1, help='Count of nodes assigned to task.')
+        parser.add_argument('-g', '--gpus_per_node', type=int, default=1, help='Count of GPU units per node.')
+        parser.add_argument('-m', '--mem_per_node', type=int, default=16, help='Memory per node [GB].')
+        parser.add_argument('-c', '--cpus_per_node', type=int, default=8, help='Count of CPU cores per node.')
+        parser.add_argument('-p', '--partition', type=str, default='plgrid-gpu',
+                            help=f'Slurm partition. Available options {self.AVAILABLE_PARTITIONS}.')
+        parser.add_argument('-j', '--job_name', type=str, required=True, help='Name of slurm job.')
+
+    def setup_hparams(self, parser: HyperOptArgumentParser) -> int:
+        """
+        Override this method to add hyperparams for grid search.
+        Every hyperparam name have to start with 'h_'.
+        See example below.
+        :param parser: Argument parser.
+        :return: Returns amount of generated slurm tasks, based on count of hyperparams combinations.
+        """
+        # parser.opt_list('--h_learning_rate', type=float, tunable=True,
+        #                 default=0.001, options=[1e-3, 1e-2, 1e-1])
+
+        # return '1' since there is no hyperparams to tune
+        return 1
+
+    def validate_args(self, args):
+        """
+        Base method which allows subclasses to validate supplied input from CLI.
+        @param args: Object with CLI args.
+        @return: None
+        """
+        pass
+
+    def convert_args(self, args):
+        """
+        Allows user to convert supplied args into different form.
+        @param args: Object with CLI args.
+        @return: None
+        """
+        pass
+
+    def configure_cluster(self, args, cluster: SlurmCluster) -> None:
+        """
+        Configures slurm cluster resources allocation.
+        :param args: Parser args.
+        :param cluster: Cluster object.
+        :return: None
+        """
+        # configure cluster
+        cluster.per_experiment_nb_nodes = args.nodes_per_exp
+        cluster.per_experiment_nb_gpus = args.gpus_per_node
+        cluster.memory_mb_per_node = args.mem_per_node * 1024
+        cluster.per_experiment_nb_cpus = args.cpus_per_node
+
+
+        # cluster.add_slurm_cmd(cmd='ntasks-per-node', value=1, comment='1 task per GPU')
+        cluster.add_slurm_cmd(cmd='constraint', value='localfs', comment='Enable local filesystem')
+        cluster.add_slurm_cmd(cmd='partition', value=args.partition, comment='Use partition dedicated for GPUs.')
+
+    @abstractmethod
+    def execute_task(self, hparams, cluster: SlurmCluster) -> None:
+        """
+        Runs training with given hparams.
+        :param hparams: Generated hparams.
+        :param cluster: SlurmCluster object.
+        :return: None
+        """
+        raise NotImplementedError()
+
+    def launch(self) -> None:
+        """
+        Runs launcher.
+        @return: None
+        """
+        self.setup_cluster_params(self._parser)
+        nb_trials = self.setup_hparams(self._parser)
+        args = self._parser.parse_args(self._args)
+        self.validate_args(args)
+        self.convert_args(args)
+
+        cluster = SlurmCluster(
+            hyperparam_optimizer=args,
+            log_path='./slurm_logs'
+        )
+        self.configure_cluster(args, cluster)
+
+        # submit a script
+        cluster.optimize_parallel_cluster_gpu(
+            self.execute_task,
+            nb_trials=nb_trials,
+            job_name=args.job_name
+        )
--- train_slurm_testtube.py
+++ train_slurm_testtube.py
@@ -1,52 +1,22 @@
 import sys
-
-from argparse import ArgumentParser
 from os.path import join
 
 from pytorch_lightning import Trainer
 from test_tube import SlurmCluster, HyperOptArgumentParser
 
-from common_utils.ArgLauncher import ArgLauncher
+from common_utils.SlurmLauncher import SlurmLauncher
 from dataset_utils.NoduleDatasetPaths import NoduleDatasetPaths
 from modules.ClsModule import ClsModule
 from modules.NoduleDataModule import NoduleDataModule
 
 
-class SlurmLauncher(ArgLauncher):
+class NihClassifierLauncher(SlurmLauncher):
 
-    def setup_parser(self, parser: ArgumentParser) -> None:
-        pass
-        parser.add_argument('-g', '--gpus_per_exp', type=int, default=2, help='Count of GPU used for one experiment.')
-        parser.add_argument('-n', '--nodes_per_exp', type=int, default=2, help='Count of nodes assigned to task.')
-
-    def execute(self, args) -> None:
-        # create cluster object
-        cluster = SlurmCluster(
-            hyperparam_optimizer=args,
-            log_path='./slurm_logs'
-        )
-
-        # configure cluster
-        cluster.per_experiment_nb_nodes = 1
-        cluster.per_experiment_nb_gpus = args.gpus_per_exp
-        cluster.memory_mb_per_node = 16000  # 8GB mem per node
-        cluster.per_experiment_nb_cpus = 8  # 8 CPU cores
-
-        cluster.add_slurm_cmd(cmd='ntasks-per-node', value=args.gpus_per_exp, comment='1 task per gpu')
-        cluster.add_slurm_cmd(cmd='constraint', value='localfs', comment='Enable local filesystem')
-        cluster.add_slurm_cmd(cmd='partition', value='plgrid-gpu', comment='Use partition dedicated for GPUs.')
-
-        # cluster.add_command('conda activate xrays')
-
-        # submit a script
-        cluster.optimize_parallel_cluster_gpu(
-            self.slurm_task,
-            nb_trials=1,
-            job_name='nodule_cls_training'
-        )
+    def setup_hparams(self, parser: HyperOptArgumentParser) -> int:
+        return 1
 
     @staticmethod
-    def slurm_task(h_params, cluster: SlurmCluster):
+    def execute_task(h_params, cluster: SlurmCluster) -> None:
         gen_name = 'test'
         print('slurm task launched')
         paths = NoduleDatasetPaths()
@@ -57,9 +27,12 @@ class SlurmLauncher(ArgLauncher):
         cls_model = ClsModule(num_classes=len(NoduleDataModule.CLS_MAPPINGS))
 
         trainer = Trainer(
-            max_epochs=3,
+            max_epochs=40,
             accelerator='ddp',
-            gpus=2
+            gpus=cluster.per_experiment_nb_gpus,
+            num_nodes=cluster.per_experiment_nb_nodes,
+            progress_bar_refresh_rate=0,
+            prepare_data_per_node=True
         )
         print('starting training')
         trainer.fit(cls_model, datamodule=cls_dm)
@@ -67,4 +40,4 @@ class SlurmLauncher(ArgLauncher):
 
 
 if __name__ == '__main__':
-    SlurmLauncher(sys.argv[1:], parser=HyperOptArgumentParser()).launch()
+    NihClassifierLauncher(sys.argv[1:]).launch()
